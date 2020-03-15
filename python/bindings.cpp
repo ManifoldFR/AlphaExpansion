8f2349e0f3ecaa4a2a61c4677c61e19a0ea7822f
ManifoldFR <manifoldfr@outlook.com>
2020-03-15 22:33:03 +0100

    Bind applyAlphaExpansion to python

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -1,9 +1,20 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <AlphaExpansion.h>
-
+#include <vector>
 
 namespace py = pybind11;
 
+std::pair<int,std::vector<int>> my_func(std::vector<std::vector<int>> potentials, std::vector<std::vector<int>> edges)
+{
+    auto res_ = applyAlphaExpansion(potentials, edges);
+    return res_;
+}
+
+
 PYBIND11_MODULE(pyalphaexpansion, m) {
     m.doc() = "Alpha expansion algorithm.";
+
+    m.def("apply_alpha_expansion", &my_func,
+          "Apply the Alpha Expansion algorithm to the input.");
 }
---
+++ python/test.py
@@ -0,0 +1,15 @@
+import pyalphaexpansion
+
+# Assemble the node and edge data
+
+with open("../data/medium/nodes.txt") as f:
+    node_data = f.readlines()
+    node_data = [list(map(int, line.split())) for line in node_data]
+
+
+with open("../data/medium/edges.txt") as f:
+    edge_data = f.readlines()
+    edge_data = [list(map(int, line.split())) for line in edge_data]
+
+result = pyalphaexpansion.apply_alpha_expansion(node_data, edge_data)
+print(result)
